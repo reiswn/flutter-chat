3c7860a5948e1f1d7f2c4cf84d0e3d02b5fc9a64
Willian <reiswn@gmail.com>
2020-01-26 19:15:32 -0300

    gitignore modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
-  Firestore.instance.collection("col").document("doc").setData({"name": "test", "bool": true});
+
+  //Singleton instance
+  //Write
+  //Firestore.instance.collection("mensagens").document().setData({"texto": "Tudo bem?", "from": "isa", "read": true});
+  //Read all doc ONE TIME
+  //QuerySnapshot snapshot = await Firestore.instance.collection("mensagens").getDocuments();
+  //snapshot.documents.forEach((f) => print(f.data));
+
+  /*
+  //Read all from collection
+  Firestore.instance.collection('mensagens').snapshots().listen((data) {
+    data.documents.forEach((doc){ print(doc.data); });
+  });
+  */
+
+  //Read all from document
+  Firestore.instance.collection('mensagens').document('msg1').snapshots().listen((data) {
+    print(data.data);
+  });
+
+
 }
 
 class MyApp extends StatelessWidget {
